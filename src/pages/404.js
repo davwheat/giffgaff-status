f11ce7e0d2a4c46ae6a1b1adf18fc32aa2068d1d
David Wheatley <hi@davwheat.dev>
2020-08-01 23:10:17 +0100

    Add 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import StatusChecker from '../components/StatusChecker'
+import Layout from '../components/Templates/Layout'
+import Link from '../components/Links/Link'
+
+const IndexPage = () => {
+  return (
+    <Layout pageTitle={'404 | giffgaff status'}>
+      <main>
+        <h1 className="gg-t-megaphone">Lost?</h1>
+        <p className="gg-t-speak">This page doesn't exist.</p>
+        <Link href="/">Go home</Link>
+      </main>
+    </Layout>
+  )
+}
+
+export default IndexPage
